f633aa6107347710cd34f387e9a80bd562c0b797
kleinmagz <kleinalboriamagallano@gmail.com>
2026-09-14 19:40:03 +0800

    fix,: add cors policy

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 5000;
 //    res.send("Hello World!");
 // });
 
-app.use(cors());
+app.use(cors({
+   origin: "https://frontend-wrs6.onrender.com"
+}));
 app.use(express.json());
 
 app.use("/todos", toDoRoutes);
