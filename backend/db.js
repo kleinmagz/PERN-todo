fb6d433981e46355edd0ca4ecb743370710ea2fb
kleinmagz <kleinalboriamagallano@gmail.com>
2026-09-14 17:22:06 +0800

    fix: reconfigured db.js to connect to neon db for serverless storage

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,23 @@
 import { Pool } from "pg";
-
+import "dotenv/config"
 // a pool manages multple client connections
 // Opening every connection for requests is slow
 // a pool keeps connections open and reuse them - efficient++
 
-const pool = new Pool({
-   user: "postgres",
-   password: "123123",
-   host: "localhost",
-   port: 5432,
-   database: "tododb"
-})
+// FOR LOCAL
+// const pool = new Pool({
+//    user: "postgres",
+//    password: "123123",
+//    host: "localhost",
+//    port: 5432,
+//    database: "tododb"
+// })
+
+// FOR NEON
+console.log("Conncecting to:", process.env.DATABASE_URL);
+const pool = new Pool ({
+   connectionString: process.env.DATABASE_URL,
+   ssl: { rejectUnauthorized: false },
+});
 
 export default pool;
